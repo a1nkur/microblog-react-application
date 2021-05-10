bd7bfc9ca65901662de3f43acc85e998875638f1
ankur29mac <ankur29mac@gmail.com>
2021-05-10 07:11:14 +0530

    added conditional loading message

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,23 +3,22 @@ import BlogListItem from "./BlogListItem";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:3344/blogs")
-    .then((response) => response.json())
-    .then(data => setBlogs(data))
+      .then(response => response.json())
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+      });
   }, []);
 
   return (
     <div className="main-content">
+      {isPending && <div>Loading...</div>}
       {/* A reusable component where a dynamic state value is passed down using props */}
-      {blogs && <BlogListItem
-        blogs={blogs}
-        title="All Blogs"
-  
-      />}
-      
+      {blogs && <BlogListItem blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
