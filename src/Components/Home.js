9bbcd913bbfcb7a9c8ac1d654f37ec66a5a809ae
ankur29mac <ankur29mac@gmail.com>
2021-05-10 16:41:59 +0530

    added error catching | conditional rendering based on error

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,18 +4,30 @@ import BlogListItem from "./BlogListItem";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3344/blogs")
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw Error("We could not fetch the data for that resource.");
+        }
+        return response.json();
+      })
       .then(data => {
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
         setIsPending(false);
       });
   }, []);
 
   return (
     <div className="main-content">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {/* A reusable component where a dynamic state value is passed down using props */}
       {blogs && <BlogListItem blogs={blogs} title="All Blogs" />}
